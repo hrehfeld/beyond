46496b2ac52a490709c6533f1e7db8d7070a6bac
Hauke Rehfeld <git@haukerehfeld.de>
2022-04-12 12:25:56 +0200

    fix: insert-state -> insertion-state

--- beyond.el
+++ beyond.el
@@ -441,9 +441,9 @@ that condition triggers."
   :group 'beyond :type '(repeat symbol))
 
 (defcustom beyond-major-mode-states
-  '((message-mode . beyond-insert-state)
+  '((message-mode . beyond-insertion-state)
     (org-agenda-mode . beyond-special-state)
-    (vterm-mode . beyond-insert-state))
+    (vterm-mode . beyond-insertion-state)
   "A alist of `(MAJOR-MODE . BEYOND-STATE)' to trigger a specific state for that major mode."
   :group 'beyond :type '(list symbol))
 
